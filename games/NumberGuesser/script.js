54b7b5edd1a76b2bd34cf2d4a7c9ef51949b57ee
Kushal Shakya <kushalshakya4114@gmail.com>
2024-10-05 16:00:07 +0545

    Completion of Project

--- games/NumberGuesser/script.js
+++ games/NumberGuesser/script.js
@@ -16,4 +16,49 @@ $(document).ready(function() {
             isDragging = false;
         }
     });
+
+    function popupModalAnalysis(){
+        let temp = -1;
+        let count = 0;
+        let staticText = ["Analysing the probability","AI mode activated.","Determining The Complexity"];
+        let randomInterval = 0;
+
+        $('.progress-loader').css("display", "block");
+        $('.dynamic-text').css("display", "block");
+        $('.guess-number-container').css("display", "none");
+        
+        function getRandomText(){
+            do{
+                randomInterval = Math.floor(Math.random() * staticText.length);
+            }while(randomInterval == temp)
+    
+            $('.dynamic-text').html(staticText[randomInterval])
+            temp = randomInterval;
+            count++;
+        }
+        getRandomText();
+        let intervalTimeout = setInterval(() => {
+            getRandomText();
+            if (count > 3) {
+                clearInterval(intervalTimeout);
+                $('.guess-number-container').css("display", "block");
+                $('.progress-loader').css("display","none");
+                $('.dynamic-text').css("display","none");
+    
+                let userNumber = $('.guesser-input').val();
+                console.log(userNumber);
+                $('.guessedNumber').html(`The Guess is number: ${userNumber}`);
+            }
+        }, 3000); 
+    }
+
+    $('.closeBtn').on('click', () => {
+        $('.dropback-effect').toggleClass('active')
+        $('.popup-modal').toggleClass('active')
+    })
+    $('.submitBtn').on('click', () => {
+        popupModalAnalysis()
+        $('.dropback-effect').toggleClass('active')
+        $('.popup-modal').toggleClass('active')
+    })
 });
